cf4703536c557664238aeab03351d7d7f28f2e24
sutaminamiura <r.miura.2718@gmail.com>
2020-03-25 19:50:07 +0900

    スペース入りファイル名対応

--- convert.sh
+++ convert.sh
@@ -2,6 +2,6 @@ mkdir output
 for FILE in *.MTS
 do
 	FILENAME=`echo ${FILE} | sed 's/\.[^\.]*$//'`
-	ffmpeg -i ${FILENAME}.MTS -vcodec copy -acodec copy ./output/${FILENAME}.mp4
-	chmod a+rwx ./output/${FILENAME}.mp4
+	ffmpeg -i "${FILENAME}.MTS" -vcodec copy -acodec copy "./output/${FILENAME}.mp4"
+	chmod a+rwx "./output/${FILENAME}.mp4"
 done
